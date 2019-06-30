19624d81bc88cccc27ddcd926abfb7b32a04db1a
doryski <dominatorskyyy@gmail.com>
2019-06-30 11:30:59 +0200

    updated readme and readiness of the file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Outlook_message
+
+Example of script that sends e-mail message using MS Outlook.
+Python, VBA and HTML were used.
--- outlook_message.py
+++ outlook_message.py
@@ -6,37 +6,37 @@ def create_message(send_From, send_To, action='display',
                    body=None, html_body=None, attachment=None):
     OutlookApp = win32.Dispatch('outlook.application')
     mail = OutlookApp.CreateItem(0)
-    
+
     mail.To = send_To
-    if send_CC != None:
+    if send_CC is not None:
         mail.CC = send_CC
-    
-    if send_BCC != None:
+
+    if send_BCC is not None:
         mail.BCC = send_BCC
-    
-    if subject != None:
+
+    if subject is not None:
         mail.Subject = subject
-    
-    if body != None:
+
+    if body is not None:
         mail.Body = body
-    
-    if html_body != None:
+
+    if html_body is not None:
         mail.HTMLBody = html_body
-    
-    if attachment != None:
+
+    if attachment is not None:
         mail.Attachments.Add(attachment)
-    
+
     try:
         mail.From = FROM
         if action.lower() == 'send':
             mail.Send()
         else:
             mail.Display()
-        
+
     except AttributeError as error:
         mail.Display()
         print('Check if argument FROM is right:', error)
-    
+
 
 ATTACHMENT = "Attachment's path"
 FROM = 'email@example.com'
@@ -44,5 +44,7 @@ TO = 'email@example.com'
 SUBJECT = "Subject"
 BODY = None
 HTML_BODY = "<h1>This is example message</h1>"
-    
-create_message(FROM, TO, action='display', subject=SUBJECT, html_body=HTML_BODY)
+
+create_message(FROM, TO, action='display',
+               subject=SUBJECT,
+               html_body=HTML_BODY)
